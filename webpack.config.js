d6ad5aea1d944cbac070dc5e7b88b1dea4bac11b
ClaySamuelC <claysamuelc@gmail.com>
2022-01-09 15:48:07 -0600

    Add development mode for compilation

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const SRC_DIR = path.join(__dirname, '/src');
 const DIST_DIR = path.join(__dirname, '/dist');
 
 module.exports = {
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'main.js',
